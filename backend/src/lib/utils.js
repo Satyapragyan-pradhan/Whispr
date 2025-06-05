9c3bd8881d74bd03fe06fe7a19a38f38637f7aaf
SatyaPragyan Pradhan <imsatya2003@gmail.com>
2025-06-05 23:26:52 +0530

    Added user auth with signup, login, and logout functionality

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+export const generateTokens = (userId, res) => {
+    const tokens = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn:"7d"
+    })
+
+    res.cookie("jwt", tokens, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,//MS
+        httpOnly: true,   //prevent XSS attack
+        sameSite: "strict", //prevents CSRF attack
+        secure:process.env.NODE_ENV!=="development"
+        
+    })
+    return tokens;
+
+}
