e1976362aaf8e77ce81fe5b865b0061f6acae79e
SatyaPragyan Pradhan <imsatya2003@gmail.com>
2025-06-24 12:51:33 +0530

    Corrected Frontend path for render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,9 @@ app.use("/api/messages", messageRoutes);
 
 const port = process.env.PORT
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
